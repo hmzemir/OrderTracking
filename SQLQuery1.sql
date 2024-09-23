910a643873faf8f9ade7cb033cc6b41b81f191bb
Hamza Emir Gündoğdu <hamzaemir614@gmail.com>
2024-09-23 20:45:16 +0300

    kullanıcılar tablosu oluşturuldu

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE Kullanıcılar (
+    u_id INT IDENTITY(1,1) PRIMARY KEY,
+    u_username NVARCHAR(50) NOT NULL UNIQUE,
+    u_password NVARCHAR(255) NOT NULL,
+    u_admin_level INT NOT NULL DEFAULT 0
+);
