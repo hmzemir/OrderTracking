4ffd70dfd511e912778ee5b1b494fff7b678d2aa
Hamza Emir Gündoğdu <hamzaemir614@gmail.com>
2024-09-23 21:00:56 +0300

    siparişler ve rotalar tabloları eklendi

---
+++ SQLQuery2.sql
@@ -0,0 +1,8 @@
+﻿
+CREATE TABLE Rotalar (
+    rota_id INT IDENTITY(1,1) PRIMARY KEY,
+    siparis_id INT NOT NULL,
+    rota_adi NVARCHAR(100) NOT NULL,
+    ucret DECIMAL(18, 2) NOT NULL,
+    FOREIGN KEY (siparis_id) REFERENCES Siparisler(id) ON DELETE CASCADE
+);
