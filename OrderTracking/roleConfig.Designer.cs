63ee79550cc5ce754726580c18556083bf79965c
Hamza Emir Gündoğdu <hamzaemir614@gmail.com>
2024-10-26 15:24:18 +0300

    roleconfig update

--- OrderTracking/roleConfig.Designer.cs
+++ OrderTracking/roleConfig.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "roleConfig";
+            label1 = new Label();
+            yetkilerCombo = new ComboBox();
+            kayitBtn = new Button();
+            sayfalarPanel = new FlowLayoutPanel();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Eras Medium ITC", 14.25F);
+            label1.Location = new Point(35, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 22);
+            label1.TabIndex = 52;
+            label1.Text = "Yetki Seç";
+            // 
+            // yetkilerCombo
+            // 
+            yetkilerCombo.Font = new Font("Eras Medium ITC", 12F, FontStyle.Italic);
+            yetkilerCombo.FormattingEnabled = true;
+            yetkilerCombo.Location = new Point(35, 50);
+            yetkilerCombo.Name = "yetkilerCombo";
+            yetkilerCombo.Size = new Size(121, 27);
+            yetkilerCombo.TabIndex = 53;
+            yetkilerCombo.SelectedIndexChanged += yetkilerCombo_SelectedIndexChanged;
+            // 
+            // kayitBtn
+            // 
+            kayitBtn.Font = new Font("Eras Medium ITC", 14.25F);
+            kayitBtn.Location = new Point(35, 467);
+            kayitBtn.Name = "kayitBtn";
+            kayitBtn.Size = new Size(79, 32);
+            kayitBtn.TabIndex = 79;
+            kayitBtn.Text = "Kaydet";
+            kayitBtn.UseVisualStyleBackColor = true;
+            kayitBtn.Click += kayitBtn_Click;
+            // 
+            // sayfalarPanel
+            // 
+            sayfalarPanel.Location = new Point(35, 97);
+            sayfalarPanel.Name = "sayfalarPanel";
+            sayfalarPanel.Size = new Size(185, 364);
+            sayfalarPanel.TabIndex = 80;
+            // 
+            // roleConfig
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(434, 520);
+            Controls.Add(sayfalarPanel);
+            Controls.Add(kayitBtn);
+            Controls.Add(yetkilerCombo);
+            Controls.Add(label1);
+            Name = "roleConfig";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "roleConfig";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label label1;
+        private ComboBox yetkilerCombo;
+        private Button kayitBtn;
+        private FlowLayoutPanel sayfalarPanel;
     }
 }
--- OrderTracking/roleConfig.cs
+++ OrderTracking/roleConfig.cs
@@ -1,20 +1,119 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace OrderTracking
 {
     public partial class roleConfig : Form
     {
+        string connectionString = ConfigurationManager.ConnectionStrings["OrderTrackingDB"].ConnectionString;
+
         public roleConfig()
         {
             InitializeComponent();
+            LoadRollerToCombo();
+        }
+
+        // Roller tablosundan verileri çek ve ComboBox'a ekle
+        private void LoadRollerToCombo()
+        {
+            string query = "SELECT rol_id, rol_isim FROM Roller";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    yetkilerCombo.Items.Add(new { Id = reader["rol_id"], Name = reader["rol_isim"] });
+                }
+                reader.Close();
+            }
+            yetkilerCombo.DisplayMember = "Name";
+            yetkilerCombo.ValueMember = "Id";
+            yetkilerCombo.SelectedIndexChanged += yetkilerCombo_SelectedIndexChanged;
+        }
+
+        // Sayfalar tablosundan checkbox oluştur ve rol izinlerini yükle
+        private void LoadSayfalarAndPermissions(int rolId)
+        {
+            sayfalarPanel.Controls.Clear(); // Önceki kontrolleri temizle
+            string query = @"
+        SELECT s.sayfa_id, s.sayfa_adi, ISNULL(ri.erisim, 0) AS erisim
+        FROM sayfalar s
+        LEFT JOIN rol_sayfa_izinleri ri ON s.sayfa_id = ri.sayfa_id AND ri.rol_id = @rolId";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@rolId", rolId);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    CheckBox cb = new CheckBox
+                    {
+                        Text = reader["sayfa_adi"].ToString(),
+                        Tag = reader["sayfa_id"],
+                        Checked = Convert.ToBoolean(reader["erisim"]),
+                        AutoSize = true // Checkbox boyutunu otomatik ayarla
+                    };
+                    sayfalarPanel.Controls.Add(cb); // FlowLayoutPanel'e checkbox ekle
+                }
+                reader.Close();
+            }
+        }
+
+
+
+
+        private void yetkilerCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (yetkilerCombo.SelectedItem != null)
+            {
+                var selectedRol = (dynamic)yetkilerCombo.SelectedItem;
+                LoadSayfalarAndPermissions((int)selectedRol.Id);
+            }
+        }
+
+        private void kayitBtn_Click(object sender, EventArgs e)
+        {
+            if (yetkilerCombo.SelectedItem == null) return;
+
+            var selectedRol = (dynamic)yetkilerCombo.SelectedItem;
+            int rolId = (int)selectedRol.Id;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlTransaction transaction = conn.BeginTransaction();
+                try
+                {
+                    SqlCommand deleteCmd = new SqlCommand("DELETE FROM rol_sayfa_izinleri WHERE rol_id = @rolId", conn, transaction);
+                    deleteCmd.Parameters.AddWithValue("@rolId", rolId);
+                    deleteCmd.ExecuteNonQuery();
+
+                    foreach (CheckBox cb in sayfalarPanel.Controls)
+                    {
+                        SqlCommand insertCmd = new SqlCommand(@"
+                            INSERT INTO rol_sayfa_izinleri (rol_id, sayfa_id, erisim) 
+                            VALUES (@rolId, @sayfaId, @erisim)", conn, transaction);
+                        insertCmd.Parameters.AddWithValue("@rolId", rolId);
+                        insertCmd.Parameters.AddWithValue("@sayfaId", (int)cb.Tag);
+                        insertCmd.Parameters.AddWithValue("@erisim", cb.Checked);
+                        insertCmd.ExecuteNonQuery();
+                    }
+
+                    transaction.Commit();
+                    MessageBox.Show("Yetki başarıyla güncellendi.");
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    MessageBox.Show("Yetki güncellenirken hata oluştu: " + ex.Message);
+                }
+            }
         }
     }
 }
